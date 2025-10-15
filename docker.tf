b2c23d7f52227b382bdc20913da1fa088b72ec39
shamvamshi <shamvamshiwork@gmail.com>
2025-10-15 11:53:31 +0530

    dockers

--- docker.tf
+++ docker.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "docker" {
-  ami           = "local.ami_id"
+  ami           = local.ami_id
 # instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
   instance_type = "t3.medium"
